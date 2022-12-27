c486f5f79740939443d7ddc6271aacbf0490e0fe
admin1 <admin1@admin1s-MacBook-Pro.local>
2022-12-27 15:00:02 +0200

    added resolved siteconfig

--- studio/schemas/siteConfig.js
+++ studio/schemas/siteConfig.js
@@ -2,10 +2,6 @@ export default {
   name: "siteconfig",
   type: "document",
   title: "Site Settings",
-  __experimental_actions: [
-    /* "create", "delete", */ "update",
-    "publish"
-  ],
   fieldsets: [
     {
       title: "SEO & metadata",
