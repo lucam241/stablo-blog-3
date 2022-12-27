414cadc5cb43c0417497de549529a3d8277c7c3e
admin1 <admin1@admin1s-MacBook-Pro.local>
2022-12-27 16:26:19 +0200

    added google ads script

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,20 @@ import "../css/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      { process.env.NEXT_PUBLIC_GOOGLE_ADS_ID &&
+        <Script
+          id="Adsense-id"
+          data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}
+          async="true"
+          strategy="beforeInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
+      }
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
