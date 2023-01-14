680edf20c882cad17b5a2f7fde748c187cae8873
admin1 <admin1@admin1s-MacBook-Pro.local>
2023-01-14 03:01:51 +0200

    adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 import "../css/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-5000031567742123"
+        async="true"
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        onError={ (e) => { console.error('Script failed to load', e) }}
+        onLoad={() => console.log('load')}
+      /> */}
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,7 @@ import Document, {
   Main,
   NextScript
 } from "next/document";
-import Script from "next/script";
-//comment
+
 class MyDocument extends Document {
   render() {
     return (
@@ -15,14 +14,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&display=swap"
             rel="stylesheet"
           />
-          <Script
-            id="Adsense-id"
-            data-ad-client="ca-pub-5000031567742123"
-            async="true"
-            strategy="beforeInteractive"
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-            onError={ (e) => { console.error('Script failed to load', e) }}
-          />
+          <script async 
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5000031567742123"
+            crossOrigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
